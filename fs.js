63342c10da3858d661c83320ef1c7ad97fd55e1a
saar120 <saar120@gmail.com>
2022-01-09 11:48:47 +0200

    fs

---
+++ fs.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// fs.writeFileSync("hi.txt", "hello !!!");
+
+// fs.appendFileSync("hi.txt", " yoyoyoyo");
+
+// fs.copyFileSync("hi.txt", "copy.txt");
+
+// fs.renameSync("hi.txt", "renamed.txt");
+
+// const filesList = [];
+// fs.readdirSync("./").forEach((item) => {
+//   filesList.push(item);
+// });
+
+// console.log(filesList);
+
+// fs.unlinkSync("copy.txt");
