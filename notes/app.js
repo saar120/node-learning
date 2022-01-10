3d319aa072079107eaa9fa6cd343016c0fa99670
saar120 <saar120@gmail.com>
2022-01-10 09:59:13 +0200

    update

--- notes/app.js
+++ notes/app.js
@@ -1,6 +1,7 @@
 const getNotes = require("./notes.js");
 const chalk = require("chalk");
 
+console.clear();
 console.log(getNotes());
 
-console.log(chalk.bold.green("Success"));
+console.log(chalk.bold.inverse.green("Success"));
