d40567c9132e372c42825cfd68c141d854f55814
saar120 <saar120@gmail.com>
2022-01-12 13:17:53 +0200

    express

--- ExpressRoutes/app.js
+++ ExpressRoutes/app.js
@@ -1,18 +1,37 @@
 const express = require("express");
+const data = require("./data");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/numbers", (req, res) => {
-  res.send("Success using get");
+  res.send(data);
 });
 app.post("/numbers", (req, res) => {
-  res.send("Success using post");
+  const num = +req.body.num;
+  if (data.includes(num)) {
+    res.send("num already included");
+    return;
+  }
+  data.push(num);
+  res.send(data);
 });
 app.delete("/numbers", (req, res) => {
-  res.send("Success using delete");
+  const num = +req.query.num;
+  if (!data.includes(num)) {
+    res.send("num not in data");
+    return;
+  }
+  data.splice(data.indexOf(num), 1);
+  res.send(data);
 });
 app.put("/numbers", (req, res) => {
-  res.send("Success using Put");
+  const numToDelete = +req.query.num;
+  const newNum = +req.body.num;
+  console.log(newNum);
+  data.splice(data.indexOf(numToDelete), 1, newNum);
+  res.send(data);
 });
 
 app.listen(3000, () => {
---
+++ ExpressRoutes/data.js
@@ -0,0 +1,3 @@
+const nums = [1, 2, 3, 4, 5, 6];
+
+module.exports = nums;
